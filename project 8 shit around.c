a8a95c781bbabf0b4f234fdd0ceaf28fbc5789a6
David Ford <david@blue-labs.org>
2018-12-02 20:28:24 -0500

    make struct definitions public for later use, comment out unused variables, indicate failure to open source file, change printninja() to use skills object

--- project 8 shit around.c
+++ project 8 shit around.c
@@ -5,29 +5,34 @@
 
 void printninja();
 
+struct map {
+    int searchval;
+    int item;
+    int found;
+};
+
+struct ninja {
+    int search;
+    int stealth;
+};
+
 int main(){
     FILE * ifp = NULL;
-    struct map {
-        int searchval;
-        int item;
-        int found;
-    };
-    struct ninja {
-        int search;
-        int stealth;
-    };
     char mapname[20];
     struct map temple[3][3];
     struct ninja skills[4];
-    int i,j,k,l, evasion, sensei;
+    int i,j;//,k,l, evasion, sensei;
 
     printf("You have arrived at the Temple!\n");
 
     while(ifp == NULL) {
-    printf("What is the name of your map?\n");
-    scanf("%s", &mapname);
+        printf("What is the name of your map?\n");
+        scanf("%s", (char *)&mapname);
 
-    ifp = fopen(mapname, "r");
+        ifp = fopen(mapname, "r");
+        if (!ifp) {
+            fprintf(stdout, "\x1b[1;31m'%s' not found or some shit\x1b[0m\n", mapname);
+        }
     }
 
     for(i=0; i<3; i++)
@@ -46,15 +51,15 @@ int main(){
 
 
     printf("You have 2 hours left to collect the relics.\n");
-    printninja();
+    printninja(&skills);
 
     return 0;
 }
 
-void printninja(){
+void printninja(struct ninja *skills){
 
     int i;
-    printf("Ninja  Search  Stealth");
+    printf("Ninja  Search  Stealth\n");
     for(i=0; i<4; i++){
         printf("%d\t", i+1);
         printf("%d\t", skills[i].search);
